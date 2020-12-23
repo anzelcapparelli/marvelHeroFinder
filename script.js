cf5d7e7f70bd9f98c0c85987c4c67354d4250d31
Eric Chen <ericchen201896@gmail.com>
2020-12-22 23:18:15 -0800

    Added in create-buttons which creates buttons based on what is stored

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ $(function () {
     var apiMarvelKey = "97a93e9e494106d892973948f5b253d9";
     var timeStamp = "1";
     var hash = "c32debe50244fc7a722036892da77e19";
+    var characterSearchHistory = ["Wolverine","Cyclops","Thor","Black Widow"];
 
-    searchComicCharacter();
-    function searchComicCharacter() {
-        var searchResult = prompt("What comic character do you want to search?");
+    // searchComicCharacter("Wolverine");
+    createButtons();
+
+    function searchComicCharacter(searchResult) {
         var marvelQueryURL = "https://gateway.marvel.com/v1/public/characters?name=" + searchResult + "&ts=" + timeStamp + "&apikey=" + apiMarvelKey + "&hash=" + hash;
         // https://gateway.marvel.com/v1/public/characters?name=cyclops&ts=1&apikey=97a93e9e494106d892973948f5b253d9&hash=c32debe50244fc7a722036892da77e19
         $.ajax({
@@ -19,13 +21,30 @@ $(function () {
             var descriptionEl = $("<div>").text(data.data.results[0].description);
             var wikiaLink = data.data.results[0].urls[1].url;
             var wikiaLinkEl = $("<a>").attr("href",wikiaLink).text("Wikia Link");
-     
             var imageLink = data.data.results[0].thumbnail.path + "." + data.data.results[0].thumbnail.extension;
             var thumbnailEl = $("<img>").attr("src",imageLink)
-    
             $(".marvelDump").append(nameEl,descriptionEl,wikiaLinkEl, thumbnailEl);
+            if(!characterSearchHistory.includes(data.data.results[0].name)) {
+                characterSearchHistory.push(data.data.results[0].name);
+            }
         })
     }
+
+
+    function createButtons() {
+        $(".characterButtonsDump").empty();
+        for(var buttonCount = 0; buttonCount < characterSearchHistory.length; buttonCount++) {
+            var characterButtonEl = $("<button>").attr("value",characterSearchHistory[buttonCount]).text(characterSearchHistory[buttonCount]);
+            characterButtonEl.attr("class","characterButton");
+            $(".characterButtonsDump").append(characterButtonEl);
+        }
+    }
+
+    $(".characterButton").on("click", function () {
+        searchComicCharacter($(this).val());
+    })
+
+
     
 
     // $.ajax({
